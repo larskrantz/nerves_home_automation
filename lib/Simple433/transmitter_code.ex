f3ca375b9f5ade353bced8349a7ffc422019f830
Lars Krantz <lars.krantz@alaz.se>
2016-06-28 15:33:46 +0200

    Fixing output for transmitter-code

--- lib/Simple433/transmitter_code.ex
+++ lib/Simple433/transmitter_code.ex
@@ -9,12 +9,14 @@ defmodule NervesHomeAutomation.Simple433.TransmitterCode do
     Converts an integer transmitter-code to a bitlist
 
     iex> NervesHomeAutomation.Simple433.TransmitterCode.to_bit_list(39291)
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1]
+    {:ok, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1]}
   """
   def to_bit_list(code) when is_integer(code) and code > 0 and code <= @maxpossibleaddress do
-    code
+    transmitter_code = code
     |> Integer.digits(2)
     |> fill_up_to_26_bits
+
+    {:ok, transmitter_code}
   end
   def to_bit_list(_) do
     {:error, "not a valid code, must be a positive integer equal or less to #{@maxpossibleaddress}."}
